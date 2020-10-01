54d6ba9ff4a955487e0236a7bd81a46b76b3ae92
{IamVks} <{vkshegde11@gmail.com}>
2020-10-01 20:21:01 +0530

    Updates

---
+++ precache-manifest.d106451f3d78fd1f75e566f38cd7d5a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b704c72f2115377acecd8e0d9000e365",
+    "url": "/Meals-Fetch-App/index.html"
+  },
+  {
+    "revision": "cc5be271274b1a138431",
+    "url": "/Meals-Fetch-App/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "200b47f0cf97640c3c52",
+    "url": "/Meals-Fetch-App/static/css/main.3f7309c2.chunk.css"
+  },
+  {
+    "revision": "cc5be271274b1a138431",
+    "url": "/Meals-Fetch-App/static/js/2.802914b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Meals-Fetch-App/static/js/2.802914b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200b47f0cf97640c3c52",
+    "url": "/Meals-Fetch-App/static/js/main.e4a9306a.chunk.js"
+  },
+  {
+    "revision": "b02dc317418f38b004c9",
+    "url": "/Meals-Fetch-App/static/js/runtime-main.93298497.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Meals-Fetch-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
